ac8c51bf7f98070b21fdfd720c5cf00660ef52ce
faithsunxngel-sketch <faithsunxngel@gmail.com>
2025-10-17 16:01:40 -0500

    Week 7 programs
    Week 7 programs

--- program_4.py
+++ program_4.py
@@ -8,3 +8,35 @@
 # Also include exception handling to deal with faulty input.
 # The distance between two points (x1,y1,z1) and (x2, y2, z2) is 
 #    given by:   sqrt ((x2-x1)^2 + (y2 - y1)^2 + (z1 - z2)^2) 
+
+import math
+
+def distance(point1, point2):
+    """Return the distance between two 3D points."""
+    x1, y1, z1 = point1
+    x2, y2, z2 = point2
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+
+
+def main():
+    print("This program is going to calculate the distance between two 3D points.")
+
+    try:
+        x1 = float(input("Enter x1: "))
+        y1 = float(input("Enter y1: "))
+        z1 = float(input("Enter z1: "))
+        point1 = (x1, y1, z1)
+
+        x2 = float(input("Enter x2: "))
+        y2 = float(input("Enter y2: "))
+        z2 = float(input("Enter z2: "))
+        point2 = (x2, y2, z2)
+        dist = distance(point1, point2)
+
+        print(f"\nThe distance between {point1} and {point2} is {dist:.2f}")
+
+    except ValueError:
+        print("\nError: Please enter valid numbers for coordinates.")
+
+if __name__ == "__main__":
+    main()
